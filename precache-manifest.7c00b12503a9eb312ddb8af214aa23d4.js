0aaf49d266020d6d14e1e243ec7f1f16bb2dfebc
Sandy Brutas <sdbrutas@toucmo.com>
2020-05-22 01:09:44 +0800

    Updates

---
+++ precache-manifest.7c00b12503a9eb312ddb8af214aa23d4.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6aedbf867f09297e0928cb7e42fcc3b",
+    "url": "/toucmo-web/index.html"
+  },
+  {
+    "revision": "13d84aff45392ab6a6ab",
+    "url": "/toucmo-web/static/css/2.fca9dd98.chunk.css"
+  },
+  {
+    "revision": "55f1b765457e698b4d98",
+    "url": "/toucmo-web/static/css/main.a90bfe3a.chunk.css"
+  },
+  {
+    "revision": "13d84aff45392ab6a6ab",
+    "url": "/toucmo-web/static/js/2.537bc99c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/toucmo-web/static/js/2.537bc99c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f1b765457e698b4d98",
+    "url": "/toucmo-web/static/js/main.d93806ef.chunk.js"
+  },
+  {
+    "revision": "a4deba0f20391c2c5336",
+    "url": "/toucmo-web/static/js/runtime-main.ab4c6e8c.js"
+  },
+  {
+    "revision": "0390258cb4f1a9ec7780f287947af35d",
+    "url": "/toucmo-web/static/media/Flaticon.0390258c.ttf"
+  },
+  {
+    "revision": "1728a7bc816b0ff04294d6b04515560e",
+    "url": "/toucmo-web/static/media/Flaticon.1728a7bc.woff2"
+  },
+  {
+    "revision": "36845183b57fef796c66b10b86f54636",
+    "url": "/toucmo-web/static/media/Flaticon.36845183.eot"
+  },
+  {
+    "revision": "3acff27f1e3bcfdcaca9f6e2da675a88",
+    "url": "/toucmo-web/static/media/Flaticon.3acff27f.svg"
+  },
+  {
+    "revision": "cefc4d29fa6c81a695db8927a5030ea1",
+    "url": "/toucmo-web/static/media/Flaticon.cefc4d29.woff"
+  },
+  {
+    "revision": "c23534acbeddbaadfd0ab2d2bbfdfc84",
+    "url": "/toucmo-web/static/media/Poppins-Bold.c23534ac.ttf"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/toucmo-web/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/toucmo-web/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "342ba3d8ac29ac8c38d7cef8efbf2dc9",
+    "url": "/toucmo-web/static/media/Poppins-SemiBold.342ba3d8.ttf"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/toucmo-web/static/media/Poppins-Thin.c0fafa83.ttf"
+  },
+  {
+    "revision": "7699c7475e2549e75700760a1ea23f50",
+    "url": "/toucmo-web/static/media/login-banner-1.7699c747.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/toucmo-web/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/toucmo-web/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/toucmo-web/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/toucmo-web/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "323b7b21f47409cf1a3ce08e37ecb7a0",
+    "url": "/toucmo-web/static/media/toucmo-logo.323b7b21.png"
+  }
+]);
